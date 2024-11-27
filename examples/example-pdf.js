4b71a38cdf462a0bc16fc7eee1cefd4d8fcc0aab
Anton Lavrevov <lavrton@gmail.com>
2024-11-26 21:01:54 -0500

    update deps, pdf sample

---
+++ examples/example-pdf.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const { createInstance } = require('../index.js');
+
+const { config } = require('dotenv');
+config();
+
+async function run() {
+  // create working instance
+  const instance = await createInstance({
+    key: process.env.POLOTNO_KEY,
+  });
+
+  // load sample json
+  const json = JSON.parse(fs.readFileSync('./test-data/private.json'));
+
+  const base64 = await instance.jsonToPDFBase64(json);
+
+  fs.writeFileSync('out.pdf', base64, 'base64');
+
+  await instance.close();
+}
+
+run();
